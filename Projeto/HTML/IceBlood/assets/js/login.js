c6e44f6b2769f435b782c88e9b20b60228fc7489
Tevezx <teveztwitch@gmail.com>
2025-10-25 12:12:38 -0300

    Adicionando header fixo e footer

--- Projeto/HTML/IceBlood/assets/js/login.js
+++ Projeto/HTML/IceBlood/assets/js/login.js
@@ -1,21 +1,3 @@
 function acessar(){
-        var keyCNPJ ='101010';
-        var keySenha = '123456';
-        var CNPJ = iptCNPJ.value;
-        var psw = iptSenha.value;
-        var email = iptEmail.value
-        if(CNPJ == ''){
-            alert ('Preencha o campo CNPJ...')
-        }else if(email == ''){
-            alert('Preencha o campo email...')
-        }
-        else if(psw == ''){
-            alert ('Preencha o campo senha...') 
-        }
-        else if(keyCNPJ != CNPJ || keySenha != psw){
-            alert('CNPJ e Senha não correspondem...')
-            return;
-        }else{
-            window.location.href = "dashboard.html";
-        }
+        
     }
