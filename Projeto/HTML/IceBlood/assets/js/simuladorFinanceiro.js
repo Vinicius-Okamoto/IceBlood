095472bdec4636ab779837f423e0e375688a8538
MateusQN <mateus.qnascimento@sptech.school>
2025-10-26 19:39:18 -0300

    Atualizando o simulador financeiro da página Institucional

--- Projeto/HTML/IceBlood/assets/js/simuladorFinanceiro.js
+++ Projeto/HTML/IceBlood/assets/js/simuladorFinanceiro.js
@@ -39,52 +39,71 @@ function publico() {
     
     
     while (nome === '' || qtdBolsa <= 0 || selectPeriodo === '') {
-        div_resultado.innerHTML = '<p>Por favor, preencha o nome, a quantidade e selecione o período!<p/>';
+        div_resultado.innerHTML = '<b>Por favor, preencha o nome, a quantidade e selecione o período!<b/>';
         return;    
     }
 
     if (selectPeriodo == 'mensal') {
        div_resultado.innerHTML = `
 
-        Olá, ${nome} ! A simulação para ${qtdBolsa} bolsas armazenadas foi realizada. <br><br>
+        Olá, ${nome}! A simulação para ${qtdBolsa} bolsas armazenadas foi realizada. <br><br>
 
-       <p><strong>SIMULAÇÃO DE PERDAS MENSAIS</strong></p>
-       <p>
-           <b>1. Sem Monitoramento (Risco Alto):</b> <br>
-           Com base na média de perdas de 20%, sua instituição descartaria aproximadamente <b style="color: red;">${perdaBolsasMensal_SemSistema.toFixed(0)}</b> bolsas mensalmente, gerando um prejuízo de <b style="color: red;">R$${custoMensal_SemSistema.toFixed(2)}</b>.
-       </p><br>
-       
-       <p>
-           <b>2. Com a Solução de Monitoramento:</b> <br>
-           Considerando uma redução de 65% no descarte (com base em estudos), sua instituição economizaria <b style="color: green;">${economiaBolsasMensal.toFixed(0)}</b> bolsas.
-       </p><br>
 
-       <p>
-           <b>ECONOMIA FINANCEIRA MENSAL:</b> <br>
-           Essa economia representa um ganho direto de <b style="color: green;">R$${economiaFinanceiraMensal.toFixed(2)}</b> no seu orçamento! A nova perda seria de <b style="color: orange;">${perdaBolsasMensal_ComSistema.toFixed(0)}</b> bolsas, com custo remanescente de R$${custoMensal_ComSistema.toFixed(2)}.
-       </p><br>
+           <b>Situação Atual (Sem Monitoramento)</b> <br>
+
+           <p>Perda Média: <i>20%</i><p/>
+           <p>Descarte Mensal: <i>${perdaBolsasMensal_SemSistema.toFixed(0)} bolsas</i><p/>
+            <p>Prejuízo: <i>R$${custoMensal_SemSistema.toFixed(2)}/mês</i><p/>
+            <p class="fonte">(Fonte: ANVISA)</p><br>
+
+       <b>Com Monitoramento</b><br>
+
+       <p>Redução de Perdas: <i>65% </i><p/>
+       <p>Bolsas Economizadas: <i>${economiaBolsasMensal.toFixed(0)} bolsas/mês</i><p/>
+       <p>Economia Financeira Direta: <i>R$${economiaFinanceiraMensal.toFixed(2)}/mês</i><p/>
+       <p class="fonte">(Fonte: CURIOUS)</p><br>
+       <b class="final">A Nova Perda Remanescente seria de <i>${perdaBolsasMensal_ComSistema.toFixed(0)} bolsas</i>, com custo de  <i>R$${economiaBolsasMensal.toFixed(2)}</i>.<b/>
+
    `;
    }
 
    if (selectPeriodo == 'anual') {
         div_resultado.innerHTML = `
-       <p><strong>SIMULAÇÃO DE PERDAS ANUAIS</strong></p>
+       Olá, ${nome}! A simulação para ${qtdBolsa} bolsas armazenadas foi realizada. <br><br>
 
-       <p>
-           <b>1. Sem Monitoramento (Risco Alto):</b> <br>
-           Com base na média de perdas de 20%, sua instituição descartaria aproximadamente <b style="color: red;">${perdaBolsasAnual_SemSistema.toFixed(0)}</b> bolsas anualmente, gerando um prejuízo de <b style="color: red;">R$${custoAnual_SemSistema.toFixed(2)}</b>.
-       </p><br>
-       
-       <p>
-           <b>2. Com a Solução de Monitoramento:</b> <br>
-           Considerando uma redução de 65% no descarte (com base em estudos), sua instituição economizaria <b style="color: green;">${economiaBolsasAnual.toFixed(0)}</b> bolsas.
-       </p><br>
 
-       <p>
-           <b>ECONOMIA FINANCEIRA MENSAL:</b> <br>
-           Essa economia representa um ganho direto de <b style="color: green;">R$${economiaFinanceiraAnual.toFixed(2)}</b> no seu orçamento! A nova perda seria de <b style="color: orange;">${perdaBolsasAnual_ComSistema.toFixed(0)}</b> bolsas, com custo remanescente de R$${custoAnual_ComSistema.toFixed(2)}.
-       </p><br>
+           <b>Situação Atual (Sem Monitoramento)</b> <br>
+
+           <p>Perda Média: <i>20%</i><p/>
+           <p>Descarte Mensal: <i>${perdaBolsasAnual_SemSistema.toFixed(0)} bolsas</i><p/>
+            <p>Prejuízo: <i>R$${custoAnual_SemSistema.toFixed(2)}/mês</i><p/>
+            <p class="fonte">(Fonte: ANVISA)</p><br>
+
+       <b>Com Monitoramento</b><br>
+
+       <p>Redução de Perdas: <i>65% </i><p/>
+       <p>Bolsas Economizadas: <i>${economiaBolsasAnual.toFixed(0)} bolsas/mês</i><p/>
+       <p>Economia Financeira Direta: <i>R$${economiaFinanceiraAnual.toFixed(2)}/mês</i><p/>
+       <p class="fonte">(Fonte: CURIOUS)</p><br>
+       <b class="final">A Nova Perda Remanescente seria de <i>${perdaBolsasAnual_ComSistema.toFixed(0)} bolsas</i>, com custo de  <i>R$${economiaBolsasAnual.toFixed(2)}</i>.<b/>
+
    `;
    }
+
+/*   Resumo de Perdas Mensais e Economia com Monitoramento
+
+Situação Atual (Sem Monitoramento - 20% de perda):
+
+Perda: 60 bolsas/mês
+
+Prejuízo: R$45.000,00/mês
+
+Com Monitoramento (Redução de 65% na perda):
+
+Economia: 39 bolsas/mês
+
+Ganho Financeiro Direto: R$29.250,00/mês
+
+Nova Perda Remanescente: 21 bolsas (Custo: R$15.750,00)*/
 
 }
