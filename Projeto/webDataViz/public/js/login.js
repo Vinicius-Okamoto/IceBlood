62758df6c787f8c11f0230bcad2de2457c2c36df
Tevezx <teveztwitch@gmail.com>
2025-12-03 19:27:18 -0300

    Atualizando as validacoes do cadastro e login

--- Projeto/webDataViz/public/js/login.js
+++ Projeto/webDataViz/public/js/login.js
@@ -15,67 +15,66 @@ function limparErros() {
 function entrar() {
     limparErros();
 
-    var erroSenha = document.getElementById('erro_senha_login');
-
     var emailVar = email_input.value;
     var senhaVar = senha_input.value;
 
-        if (emailVar == "" || senhaVar == "") {
-            erroSenha.innerHTML = "Mensagem de erro para todos os campos em branco";
-            return false;
-        }
-        else {
-            setInterval(5000)
-        }
+    if (emailVar == "") {
+        document.getElementById('erro_email_login').innerHTML = "Preencha o campo email.";
+        document.getElementById('erro_email_login').style.display = "block";
+    }
 
-        console.log("FORM LOGIN: ", emailVar);
-        console.log("FORM SENHA: ", senhaVar);
+    if (senhaVar == "") {
+        document.getElementById('erro_senha_login').innerHTML = "Preencha o campo senha.";
+        document.getElementById('erro_senha_login').style.display = "block";
+    }
 
-        fetch("/usuarios/autenticar", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                emailServer: emailVar,
-                senhaServer: senhaVar
-            })
-        }).then(function (resposta) {
-            console.log("ESTOU NO THEN DO entrar()!")
+    console.log("FORM LOGIN: ", emailVar);
+    console.log("FORM SENHA: ", senhaVar);
 
-            if (resposta.ok) {
-                console.log(resposta);
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
 
-                resposta.json().then(json => {
-                    console.log(json);
-                    console.log(JSON.stringify(json));
-                    sessionStorage.EMAIL_USUARIO = json.email;
-                    sessionStorage.NOME_USUARIO = json.nome;
-                    sessionStorage.ID_USUARIO = json.id;
+        if (resposta.ok) {
+            console.log(resposta);
 
-                     if (emailVar == `suporte@gmail.com` || emailVar == `suporte2@gmail.com`) {
-                         setTimeout(function () {
-                         window.location = "./dashboard/macroDashSuporte.html";
-                     }, 1000);
-                     } else {
-                         setTimeout(function () {
-                        
-                         window.location = "./dashboard/macroDash.html";
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.NOME_USUARIO = json.nome;
+                sessionStorage.ID_USUARIO = json.id;
+
+                if (emailVar == `suporte@gmail.com` || emailVar == `suporte2@gmail.com`) {
+                    setTimeout(function () {
+                        window.location = "./dashboard/macroDashSuporte.html";
+                    }, 1000);
+                } else {
+                    setTimeout(function () {
+                        window.location = "./dashboard/macroDash.html";
                     }, 1000);
 
-                    }
-                });
+                }
+            });
 
-            } else {
-                console.log("Houve um erro ao tentar realizar o login!");
-                resposta.text().then(texto => {
-                    console.error(texto);
-                });
-            }
+        } else {
+            console.log("Houve um erro ao tentar realizar o login!");
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
 
-        }).catch(function (erro) {
-            console.log(erro);
-        })
+    }).catch(function (erro) {
+        console.log(erro);
+    })
 
-        return false;
+    return false;
 }
