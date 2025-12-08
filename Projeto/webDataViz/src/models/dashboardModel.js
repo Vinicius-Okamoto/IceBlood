bcedf4430fb57a19526b000ef6150ae6c05fea64
arthurrbispo <arthur.bsantos@sptech.school>
2025-12-08 12:16:56 -0300

    Atualizando Dashboard

--- Projeto/webDataViz/src/models/dashboardModel.js
+++ Projeto/webDataViz/src/models/dashboardModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function buscarTemperaturas(token, idCamara) {
     var instrucao = `
-        SELECT * FROM vwDashboard WHERE unidadeToken = '${token}' AND idCamara = ${idCamara} ;
+        SELECT * FROM vwDashboard WHERE unidadeToken = '${token}' AND idCamara = ${idCamara};
     `;
 
     return database.executar(instrucao);
